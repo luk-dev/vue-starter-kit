115f482fab2a3673b2a1384b13b9e610ac59f403
Łukasz <lukasz.dabik@gmail.com>
2019-07-06 10:03:46 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <the-navigation/>
+    <div>
+      Change Language to:
+      <TheLanguageSwitcher/>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import TheLanguageSwitcher from '@/components/TheLanguageSwitcher'
+import TheNavigation from '@/components/TheNavigation'
+
+export default {
+  name: 'app',
+  components: {
+    TheLanguageSwitcher,
+    TheNavigation
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "/assets/scss/style.scss";
+</style>
---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ $t('welcome_message') }}</h1>
+    <h2>{{ $t('popular_links') }}</h2>
+    <ul class="popular_links">
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.popular_links {
+  margin-top: 4rem;
+}
+</style>
